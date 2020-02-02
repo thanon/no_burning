e2abf5978a991995d2adeabbbb0c2cd3e81a6b84
thanon <18073217+thanon@users.noreply.github.com>
2020-02-02 18:03:24 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,32 +1,51 @@
+// Variables
 const navElement = document.querySelectorAll('section')
 const navList = document.getElementById('navbarlist')
 
+// Navlist Iterations
 navElement.forEach(element => {
   const navlistElement = `<li class='c_menu_style${element.className}'data-link=${element.id}><a href="#${element.id}">${element.dataset.nav}</li>`
   navList.insertAdjacentHTML('beforeend', navlistElement)
 })
 
+// Scroll to section on click by listenting to the click-event in navlist
 navList.addEventListener('click', e => {
   e.preventDefault()
-  const elementtarget = e.target.hasAttribute('data-link') 
+  const elementTarget = e.target.hasAttribute('data-link') 
   ? e.target : e.target.parentElement
-  const scrolltoElement = document.getElementById(elementtarget.dataset.link)
-  scrolltoElement.scrollIntoView({behavior: 'smooth', block: 'start'})
+  const scrolltoElement = document.getElementById(elementTarget.dataset.link)
+  scrolltoElement.scrollIntoView({behavior: 'smooth', block: 'end'})
 });
 
+//Pass css class 'active'to Navbar and to section by using IntersectionObserver
 const callback = entries => {
   entries.forEach(entry => {
     const navlistElement = document.querySelector(`.c_menu_style[data-link='${entry.target.id}']`,)
     const section = document.getElementById(entry.target.id)
 
     if (entry && entry.isIntersecting) {
-      navListElement.classList.add('active_li')
-      section.classList.add('active_li')
-    }
-    // else if  (section.classList.contains('active_li') && navlistElement.classList.contains('active_li')){
-    //   navListElement.classList.remove('active_li')
-    // } 
+      navlistElement.classList.add('active')
+      section.classList.add('active')
+    } else {
+      if (navlistElement.classList.contains('active')) {
+        navlistElement.classList.remove('active')
+      }
+
+      if (section.classList.contains('active')) {
+        section.classList.remove('active')
+      }
+    }  
     })
 }
 
+// Options for the observer. 
+const options = {
+  threshold: 0.6
+};
+
+// Setting an observer with options and callback when checks
+const observer = new IntersectionObserver(callback, options)
+navElement.forEach(element => {
+  observer.observe(document.getElementById(element.id))
+})
 
